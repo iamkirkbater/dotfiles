b13831a693f473fca0dcd8b8d17aea7dfdaa438f
Kirk Bater <kxbtwc@rit.edu>
2016-07-13 17:11:54 -0400

    updated install for vim plugins install

--- init.sh
+++ init.sh
@@ -6,3 +6,5 @@ ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+
+vim +PluginInstall +qall
