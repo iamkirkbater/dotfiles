c6172ab7c915c51f758493b6bbb374a67beb40e8
Kirk Bater <kirk.bater@gmail.com>
2026-07-01 16:00:20 -0400

    updates init to handle config directory

--- init.sh
+++ init.sh
@@ -10,4 +10,17 @@ ln -sf $(pwd)/.vimrc $HOME/.vimrc
 
 ln -sf $(pwd)/config.ghostty $HOME/Library/Application\ Support/com.mitchellh.ghostty/config.ghostty
 
+config_dirs=$(find config/* -type d)
+config_files=$(find config/* -type f)
+
+mkdir -p $HOME/.config
+
+for dir in $config_dirs; do
+  mkdir -p $HOME/.$dir
+done
+
+for file in $config_files; do
+  ln -sf $(pwd)/$file $HOME/.$file
+done
+
 vim +PluginInstall +qall
